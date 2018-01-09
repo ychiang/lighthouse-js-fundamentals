3de95dcb65e294a65b489ff64e13a0851c51e413
DrEaM <gracechiang@me.com>
2018-01-09 00:06:02 -0800

    Adds ingredients

---
+++ ingredients.js
@@ -0,0 +1,14 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+var repeat = 0;
+while (repeat < 8) {
+	console.log(ingredients[repeat]);
+	repeat++;
+}
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+for (var i = 0; i<8; i++) {
+	console.log(ingredients[i]);
+}
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+for (var i = 7; i>=0; i--) {
+	console.log(ingredients[i]);
+}
